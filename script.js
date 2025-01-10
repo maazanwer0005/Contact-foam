da5913ccfa1e640c3cce4e7729e6d0a71927bca6
Maaz Anwer <maazanwer003@gmail.com>
2025-01-10 11:58:47 +0500

    some things changes

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-  <script>
-  function submit(){
-      let userName = document.getElementById("username").value;
-      let email = document.getElementById("email").value;
-      let message = document.getElementById("password").value;
-      let message = document.getElementById("message").value
+function submit(){
+    let username = document.getElementById("username").value;
+    let useremail = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("message").value;
 
-      let params = {
-          full_name: fullName,
-          subject: subject,
-          email: userEmail,
-          message: message
-      }
-      emailjs.send("service_o99m92q", "template_1u6o39r", params).then((res) => {
-          alert("Query Sent");
-          console.log(res)
-      })
-      .catch((error) => {console.log(error)})
-  }
-</script>
+    let params = {
+        Full_Name: username,
+        Email: useremail,
+        Password: password,
+        Message: message
+    }
+    emailjs.send("service_1t3i2r5", "template_2r0jshz", params).then((res) => {
+        alert("Query Sent")
+        console.log(res)
+    })
+    .catch((error) => {console.log(error)})
+}
